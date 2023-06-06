1e980a95bf3252f800126e68bc54b5ffc3b14bfa
belloemmanuel <belloemmanuel02@gmail.com>
2023-06-06 11:54:29 +0100

    final css edit

--- main.js
+++ main.js
@@ -16,14 +16,8 @@ const progressContainer = document.getElementById('progress-container');
 
 
 let songs = ['audio1', 'audio2', 'audio3']
-let songsInfo = {
-    artistes : ['Joe boy_Feat Ckay', 'Joe boy_Feat Ludacris', 'Adekunle Gold'],
-    titles: ['Wetin Be Love', 'Chiken Spice and Curry', 'Do you Mind']
-}
-
 let song = songs[songs.length-3]  //0
 let songIndex = songs.length; //3
-
 
 const defaultSong = () =>{
     audio.src = `music/${song}.mp3`;
@@ -79,20 +73,80 @@ let setProgress = (e) => {
 };
 
 let randomFunc = () => {
-    let randomNumber = Math.floor(Math.random() * songs.length);
+    randomNumber = Math.floor(Math.random() * songs.length);
     song = songs[randomNumber]
     defaultSong()
     playSong();
 };
+
+let getDuration = (e) =>{
+        const {duration,currentTime} = e.srcElement;
+        var sec;
+        var sec_d;
+    
+        // define minutes currentTime
+        let min = (currentTime==null)? 0:
+         Math.floor(currentTime/60);
+         min = min <10 ? '0'+min:min;
+    
+        // define seconds currentTime
+        function get_sec (x) {
+            if(Math.floor(x) >= 60){
+                
+                for (var i = 1; i<=60; i++){
+                    if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+                        sec = Math.floor(x) - (60*i);
+                        sec = sec <10 ? '0'+sec:sec;
+                    }
+                }
+            }else{
+                 sec = Math.floor(x);
+                 sec = sec <10 ? '0'+sec:sec;
+             }
+        } 
+    
+        get_sec (currentTime,sec);
+    
+        // change currentTime DOM
+        musicTime.innerHTML = min +':'+ sec;
+    
+        // define minutes duration
+        let min_d = (isNaN(duration) === true)? '0':
+            Math.floor(duration/60);
+         min_d = min_d <10 ? '0'+min_d:min_d;
+    
+    
+         function get_sec_d (x) {
+            if(Math.floor(x) >= 60){
+                
+                for (var i = 1; i<=60; i++){
+                    if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+                        sec_d = Math.floor(x) - (60*i);
+                        sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+                    }
+                }
+            }else{
+                 sec_d = (isNaN(duration) === true)? '0':
+                 Math.floor(x);
+                 sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+             }
+        } 
+    
+        // define seconds duration
+        
+        get_sec_d (duration);
+    
+        // change duration DOM
+        console.log(min_d +':'+ sec_d)
+    
+};
+
 randomBtn.addEventListener("click", randomFunc);
-
 audio.addEventListener("ended", nextSong);
-
-
 progressContainer.addEventListener("click", setProgress);
 audio.addEventListener("timeupdate", updateProgress);
-
 pause.addEventListener('click', pauseSong)
 play.addEventListener('click', playSong)
 next.addEventListener('click', nextSong)
 prev.addEventListener('click', prevSong)
+audio.addEventListener('timeupdate',getDuration); 
